2a30192860e3f8b56eec911f043c2a47dd44fee9
avl <avl@itvsystems.com.ua>
2026-09-17 11:52:27 +0300

    fix rgb

--- src/Light_music.ino
+++ src/Light_music.ino
@@ -100,7 +100,7 @@ void logLinef(const char* fmt, ...) {
 #define LED_PIN     4
 #define NUM_LEDS    18           // <-- 54 фізичних LED / 3 на піксель (12V WS2811-стрічка)
 #define LED_TYPE    WS2812B
-#define COLOR_ORDER GRB
+#define COLOR_ORDER RGB
 
 uint8_t currentBrightness = 15; // 0-255, тепер керується з вебсторінки
 
